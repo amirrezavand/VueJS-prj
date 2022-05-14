32cc33fa47c01df5df3cb2aa04b1fae8430f3ae2
rezavand <ar.rezavand@gmail.com>
2022-05-14 17:01:55 +0430

    we need to use components in Vue js. Basic introduction to Components in Vue js

---
+++ prj23/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data(){
+    return {
+      hobbies : ['Reading','Writing','Playing']
+    };
+  }
+});
+
+app.component('hobbies-component',{
+  template: `<ul class="list-group">
+              <li class="list-group-item" v-for="hobby in hobbies">
+                {{hobby}}
+              </li>
+            </ul>`,
+  data(){
+    return {
+      hobbies : ['Reading','Writing','Playing']
+    };
+  }
+})
+
+app.mount("#app");
+
+
+
+
+
+
