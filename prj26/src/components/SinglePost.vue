b5e4af962fec9d397b0a64c04cfb317ef415965f
rezavand <ar.rezavand@gmail.com>
2022-05-19 14:40:32 +0430

    Perform Parent to Child Communication using this Props in Vuejs

---
+++ prj26/src/components/SinglePost.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="title">{{post.title}}</div>
+        <div>{{post.description}}</div>
+        <a href="" @click.prevent="changeTitle()">Click here to change title</a>
+    </div>
+</template>
+
+
+<script>
+
+export default ({
+    props: ['data','isActive'],
+    data() {
+        return {
+            post : this.data
+        }
+    },
+    methods: {
+        changeTitle(){
+            this.post.title=this.post.title+'+';
+        }
+    }
+})
+</script>
