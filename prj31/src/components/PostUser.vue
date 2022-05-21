f5d45ebb2b54cf77b1d31a75879fa20ba8225184
rezavand <ar.rezavand@gmail.com>
2022-05-21 19:45:21 +0430

    rovide / Inject for parent-child communication. Apply reactivity for provide values

---
+++ prj31/src/components/PostUser.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      {{ user.value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // props : ['user'],
+    inject : ['user']
+}
+</script>
