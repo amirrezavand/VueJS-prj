e78f85417d9cccc60126576c8016060167df482d
rezavand <ar.rezavand@gmail.com>
2022-05-22 17:35:37 +0430

    Apply Provide - Inject for the Custom Event Methods. Props Vs Provide in Vue js

---
+++ prj32/src/components/PostUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      {{ user.value }}
+    </div>
+  <div>
+    <a href="" @click.prevent="changeuser">Change User</a>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // props : ['user'],
+    inject : ['user','changeuser']
+}
+</script>
